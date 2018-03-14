dc008a396da8aa6f87fc9435cd1a80f8f3a4e3f1
mukailritonga <mukailritonga92@gmail.com>
2018-03-14 10:17:01 +0700

    template

---
+++ header.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from ansonika.com/findoctor/menu_2/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 30 Nov 2017 15:46:52 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="uituit">
+    <title>Bank Prekreditan Rakyat, Kredit Murah, Lelang Propery www.uituit.com</title>
+    <meta name="keywords" content="Bank BPR, Kredit Murah,Kredit Mobil, Lelang Property, Lelang Bank, BPR Kredit, KPR BPR, Bank Perkreditan Murah, Kredit Bunga Rendah">
+    <meta name="description" content="Portal pengajuan kredit, tabungan dan deposit agar mem permudah para konsumen dapat berhubungan langusng dengan bank-bank atau nonbank. juga memberikan kemudahan konsumen menemukan / mengetahui area terdekat bank-bank atau nonbank.">
+    <meta charset="utf-8">
+    <meta name="Publisher" content="PT OTRET DOT COM" />
+    <meta name="Robots" content="All" />
+    <meta name="Page-topic" content="Bank Prekreditan Rakyat | Kredit Murah | Kredit Bunga Rendah | Lelang Property" />
+    <meta name="Rating" content="General" />
+    <meta name="Revisit-after" content="1 days" />
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+
+    <!-- BASE CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/menu.css" rel="stylesheet">
+    <link href="css/vendors.css" rel="stylesheet">
+    <link href="css/icon_fonts/css/all_icons_min.css" rel="stylesheet">
+
+    <!-- YOUR CUSTOM CSS -->
+    <link href="css/custom.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div class="layer"></div>
+<!-- Mobile menu overlay mask -->
+
+<div id="preloader">
+    <div data-loader="circle-side"></div>
+</div>
+<!-- End Preload -->
+
+<header class="header_sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-6">
+                <div id="logo_home">
+                    <h1><a href="index.php" title="uituit.com">uituit.com</a></h1>
+                </div>
+            </div>
+            <nav class="col-lg-9 col-6">
+                <a class="cmn-toggle-switch cmn-toggle-switch__htx open_close" href="#0"><span>Menu mobile</span></a>
+                <ul id="top_access">
+                    <li><a href="login.php"><i class="pe-7s-user"></i></a></li>
+
+                </ul>
+                <div class="main-menu">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="lelang.php">Lelang</a></li>
+                        <li><a href="bank.php">Bank & Nonbank</a></li>
+                        <li><a href="blog.php">Blog</a></li>
+                        <li><a href="About">Joint us a bank</a></li>
+
+                    </ul>
+                </div>
+                <!-- /main-menu -->
+            </nav>
+        </div>
+    </div>
+    <!-- /container -->
+</header>
+<!-- /header -->
---
+++ index.php
@@ -0,0 +1,323 @@
+<?php include "header.php"; ?>
+
+<main>
+    <div class="hero_home version_1">
+        <div class="content">
+            <h3>uituit.com</h3>
+            <p>
+                Temukan produk financial kredit, tabungan, dan deposito terbaik buat anda.
+            </p>
+            <form method="post" action="http://ansonika.com/findoctor/menu_2/list.html">
+                <div id="custom-search-input">
+                    <ul>
+                        <li>
+                            <input type="radio" id="all" name="radio_search" value="all" checked>
+                            <label for="all">Kredit</label>
+                        </li>
+                        <li>
+                            <input type="radio" id="doctor" name="radio_search" value="doctor">
+                            <label for="doctor">Tabungan</label>
+                        </li>
+                        <li>
+                            <input type="radio" id="clinic" name="radio_search" value="clinic">
+                            <label for="clinic">Deposito</label>
+                        </li>
+                    </ul>
+                    <div class="input-group">
+                        <input type="text" class=" search-query" placeholder="Masukan Nominal..">
+                        <input type="submit" class="btn_search" value="Search">
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- /Hero -->
+
+    <div class="container margin_120_95">
+        <div class="main_title">
+            <h2>Kemudahan <strong>Menggunakan </strong> uituit.com !</h2>
+            <p>uituit.com senang membantu Anda untuk mencari dan mengajukan produk finansial perbankkan tanpa dikenakan biaya / gratis Rp.0.
+            </p>
+        </div>
+        <div class="row add_bottom_30">
+            <div class="col-lg-4">
+                <div class="box_feat" id="icon_1">
+                    <span></span>
+                    <h3>Cari Produk</h3>
+                    <p>Mencari mitra terbaik uiuit.com meliputi lembaga keuangan perbankan dan nonbank di daerah terdekat anda.</p>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="box_feat" id="icon_2">
+                    <span></span>
+                    <h3>Bandingan</h3>
+                    <p>Menemukan produk-produk financial sesuai kebutuhan anda dan menemulan lokasi bank-bank atau nonbank yang terdekat.</p>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="box_feat" id="icon_3">
+                    <h3>Ajukan</h3>
+                    <p>Pengajuan mudah cukup mengisi data diri anda, dan beberapa jam kedepan akan di hub oleh perbankkan.</p>
+                </div>
+            </div>
+        </div>
+        <!-- /row -->
+        <p class="text-center"><a href="bank.php" class="btn_1 medium">Cari Bank & Nonbank</a></p>
+
+    </div>
+    <!-- /container -->
+    <div class="bg_color_1">
+        <div class="container margin_120_95">
+            <div class="main_title">
+                <h2>Recommend</h2>
+                <p>Usu habeo equidem sanctus no. Suas summo id sed, erat erant oporteat cu pri.</p>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="box_list home">
+
+                        <figure>
+
+                            <a href="detail-page.html"><img src="img/bank1.jpg" class="img-fluid" alt=""></a>
+                            <div class="preview"><span>Read more</span></div>
+                        </figure>
+                        <div class="wrapper">
+
+                            <h3>Bank Perkreditan Rakyat</h3>
+                            <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cuodo....</p>
+                            <span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
+                            <a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="img/badges/badge_1.svg" width="15" height="15" alt=""></a>
+                        </div>
+                        <ul>
+                            <li><i class="icon-eye-7"></i> 854 Views</li>
+                            <li><a href="detail-page.html">Ajaukan</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="box_list home">
+
+                        <figure>
+
+                            <a href="detail-page.html"><img src="img/bank2.jpg" class="img-fluid" alt=""></a>
+                            <div class="preview"><span>Read more</span></div>
+                        </figure>
+                        <div class="wrapper">
+
+                            <h3>Bank Perkreditan Rakyat</h3>
+                            <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cuodo....</p>
+                            <span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
+                            <a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="img/badges/badge_1.svg" width="15" height="15" alt=""></a>
+                        </div>
+                        <ul>
+                            <li><i class="icon-eye-7"></i> 854 Views</li>
+                            <li><a href="detail-page.html">Ajaukan</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="box_list home">
+
+                        <figure>
+
+                            <a href="detail-page.html"><img src="img/bank3.jpg" class="img-fluid" alt=""></a>
+                            <div class="preview"><span>Read more</span></div>
+                        </figure>
+                        <div class="wrapper">
+
+                            <h3>Bank Perkreditan Rakyat</h3>
+                            <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cuodo....</p>
+                            <span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
+                            <a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="img/badges/badge_2.svg" width="15" height="15" alt=""></a>
+                        </div>
+                        <ul>
+                            <li><i class="icon-eye-7"></i> 854 Views</li>
+                            <li><a href="detail-page.html">Ajaukan</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="box_list home">
+
+                        <figure>
+
+                            <a href="detail-page.html"><img src="img/bank2.jpg" class="img-fluid" alt=""></a>
+                            <div class="preview"><span>Read more</span></div>
+                        </figure>
+                        <div class="wrapper">
+
+                            <h3>Bank Perkreditan Rakyat</h3>
+                            <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cuodo....</p>
+                            <span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
+                            <a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="img/badges/badge_6.svg" width="15" height="15" alt=""></a>
+                        </div>
+                        <ul>
+                            <li><i class="icon-eye-7"></i> 854 Views</li>
+                            <li><a href="detail-page.html">Ajaukan</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="box_list home">
+
+                        <figure>
+
+                            <a href="detail-page.html"><img src="img/bank3.jpg" class="img-fluid" alt=""></a>
+                            <div class="preview"><span>Read more</span></div>
+                        </figure>
+                        <div class="wrapper">
+
+                            <h3>Bank Perkreditan Rakyat</h3>
+                            <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cuodo....</p>
+                            <span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
+                            <a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="img/badges/badge_6.svg" width="15" height="15" alt=""></a>
+                        </div>
+                        <ul>
+                            <li><i class="icon-eye-7"></i> 854 Views</li>
+                            <li><a href="detail-page.html">Ajaukan</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="box_list home">
+
+                        <figure>
+
+                            <a href="detail-page.html"><img src="img/bank1.jpg" class="img-fluid" alt=""></a>
+                            <div class="preview"><span>Read more</span></div>
+                        </figure>
+                        <div class="wrapper">
+
+                            <h3>Bank Perkreditan Rakyat</h3>
+                            <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cuodo....</p>
+                            <span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
+                            <a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="img/badges/badge_1.svg" width="15" height="15" alt=""></a>
+                        </div>
+                        <ul>
+                            <li><i class="icon-eye-7"></i> 854 Views</li>
+                            <li><a href="detail-page.html">Ajaukan</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- /row -->
+        </div>
+        <!-- /container -->
+    </div>
+    <!-- /white_bg -->
+
+
+    <!-- /container -->
+
+    <div id="app_section">
+        <div class="container">
+            <div class="row justify-content-around">
+                <div class="col-md-5">
+                    <p><img src="img/app_img.svg" alt="" class="img-fluid" width="500" height="433"></p>
+                </div>
+                <div class="col-md-6">
+                    <small>Application</small>
+                    <h3>Download <strong>Findoctor App</strong> Now!</h3>
+                    <p class="lead">Tota omittantur necessitatibus mei ei. Quo paulo perfecto eu, errem percipit ponderum no eos. Has eu mazim sensibus. Ad nonumes dissentiunt qui, ei menandri electram eos. Nam iisque consequuntur cu.</p>
+                    <div class="app_buttons wow" data-wow-offset="100">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 43.1 85.9" style="enable-background:new 0 0 43.1 85.9;" xml:space="preserve">
+							<path stroke-linecap="round" stroke-linejoin="round" class="st0 draw-arrow" d="M11.3,2.5c-5.8,5-8.7,12.7-9,20.3s2,15.1,5.3,22c6.7,14,18,25.8,31.7,33.1" />
+                            <path stroke-linecap="round" stroke-linejoin="round" class="draw-arrow tail-1" d="M40.6,78.1C39,71.3,37.2,64.6,35.2,58" />
+                            <path stroke-linecap="round" stroke-linejoin="round" class="draw-arrow tail-2" d="M39.8,78.5c-7.2,1.7-14.3,3.3-21.5,4.9" />
+						</svg>
+                        <a href="#0" class="fadeIn"><img src="img/apple_app.png" alt="" width="150" height="50" data-retina="true"></a>
+                        <a href="#0" class="fadeIn"><img src="img/google_play_app.png" alt="" width="150" height="50" data-retina="true"></a>
+                    </div>
+                </div>
+            </div>
+            <!-- /row -->
+        </div>
+        <!-- /container -->
+    </div>
+    <!-- /app_section -->
+</main>
+<!-- /main content -->
+
+<footer>
+    <div class="container margin_60_35">
+        <div class="row">
+            <div class="col-lg-3 col-md-12">
+                <p>
+                    <a href="index.html" title="Findoctor">
+                        <img src="img/logo.png" data-retina="true" alt="" width="163" height="36" class="img-fluid">
+                    </a>
+                </p>
+            </div>
+            <div class="col-lg-3 col-md-4">
+                <h5>About</h5>
+                <ul class="links">
+                    <li><a href="#0">About us</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="#0">FAQ</a></li>
+                    <li><a href="login.html">Login</a></li>
+                    <li><a href="register.html">Register</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-4">
+                <h5>Useful links</h5>
+                <ul class="links">
+                    <li><a href="#0">Doctors</a></li>
+                    <li><a href="#0">Clinics</a></li>
+                    <li><a href="#0">Specialization</a></li>
+                    <li><a href="#0">Join as a Doctor</a></li>
+                    <li><a href="#0">Download App</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-4">
+                <h5>Contact with Us</h5>
+                <ul class="contacts">
+                    <li><a href="tel://61280932400"><i class="icon_mobile"></i> + 61 23 8093 3400</a></li>
+                    <li><a href="mailto:info@findoctor.com"><i class="icon_mail_alt"></i> help@findoctor.com</a></li>
+                </ul>
+                <div class="follow_us">
+                    <h5>Follow us</h5>
+                    <ul>
+                        <li><a href="#0"><i class="social_facebook"></i></a></li>
+                        <li><a href="#0"><i class="social_twitter"></i></a></li>
+                        <li><a href="#0"><i class="social_linkedin"></i></a></li>
+                        <li><a href="#0"><i class="social_instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!--/row-->
+        <hr>
+        <div class="row">
+            <div class="col-md-8">
+                <ul id="additional_links">
+                    <li><a href="#0">Terms and conditions</a></li>
+                    <li><a href="#0">Privacy</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <div id="copy">© 2017 Findoctor</div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--/footer-->
+
+<div id="toTop"></div>
+<!-- Back to top button -->
+
+<!-- COMMON SCRIPTS -->
+<script src="js/jquery-2.2.4.min.js"></script>
+<script src="js/common_scripts.min.js"></script>
+<script src="js/functions.js"></script>
+
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs2.uzone.id/2fn7a2/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582HVlH3eBnL31T%2fAyTqlQFOvhL5cnYc0ZpBVbg9CDNUXDpRDugHmtFhArs0W5B3tgrN13aXzIi5E8vX4cs1J9safHJBf8HEsgNQHyn%2fquPzADvntAyOA7SZKCF6z5GXGUg6%2bAsWjoJCEvft8XaaHzjLuuyWzDLM3esOxixh1XKNAdf0cF9QkIcDZkLgq7PO2%2bo3dqBMTzD6vt9Pt%2bEY56egL6DvPbrRn72GY25q4BUvR8lEH%2f1KFueHovyNyPmMh2esDyoMQ078iepIyuOGcsdPsmGqDINkT5dTY3iY0BJp9yeVcA3P8beexvwQUBFn9L6TX2fpObH7GcVOvpvahW4hcyoX00bmHJTUBURFAlJs96iyug%2faI6Y3NtAvWgjWvSm1Q5qKNbWgDIrvmkhF0VPN68W5VEhfN34rkyXH1cYyPtHntm4Z%2bvLLHiBlI9s2uSZO7nV0958KqhIumZiSYZTyxfp7gvs4fa0FLS0DE3tBLTIVxpgW2pcyZAyUwfFDtpcw%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+
+
+<!-- Mirrored from ansonika.com/findoctor/menu_2/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 30 Nov 2017 15:47:41 GMT -->
+</html>
